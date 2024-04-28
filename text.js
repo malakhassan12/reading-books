20a13f141dc6179abdd0f183d3bef6a331203a79
malakhassan12 <158052165+malakhassan12@users.noreply.github.com>
2024-04-28 04:52:43 -0700

    Add files via upload

---
+++ text.js
@@ -0,0 +1,196 @@
+let lang={
+    ar:{
+        two_start_section_h1:"اهلا بكم في قسم ذوي الاحتياجات الخاصه",
+        two_start_section_p:"لغضب بسبب إعاقتي هو مضيعة للوقت، يجب على المرء أن يمضي قدمًا ويتقدم في الحياة، إذا كنت غاضبًا أو متذمرًا باستمرار فلن يكون لدى الناس الوقت لك.",
+        content:"المحتوى",
+        about:"عن",
+        podcast:"التدوين الصوتي",
+        contact:"للتواصل",
+        back:"العوده",
+        two_content_p1:"أصحاب الهمم هم أشخاص يستطيعون صنع المعجزات، يضيئون الحياة، وهم نجوم العالم، ويتمتعون بطاقة فريدة",
+        two_content_p2:"إذا لم تختلط بأصحاب الهمم،لن تعرف قيمة الحب لأنهم يملكون ما يكفي من السكينة ليكونوا آمنين وسعداء",
+        two_content_a:"اقرأ المزيد",
+        question:"اقرأ",
+        two_about_p1:"أصحاب الهمم وذوي الإعاقة هم النجوم القادرون على إضاءة العالم",
+        two_about_p2:"كل البشر مسؤولون عن سكن الأرض وخلق الحياة، فلا أحد بلا دور",
+        two_about_p3:"حظاً موفقاً أيها صاحب العزيمة. هناك معجزة في الأرض وفي السماء، عجزك يذهب إلى السماء قبلك.",
+        two_about_p4:"من أعظم قيم الحياة تقديس الإنسان وتقديس نفسه واحترام كل ما خلقه الله",
+        two_podcast_h1:"التدوين الصوتي",
+        two_contact_h3:"للتواصل معنا",
+        /*------------------------------------------------------two---------------------------------------------------*/ 
+        index_intro_h2:"استمتع بالقراءة",
+        index_intro_around:"حول",
+        index_intro_content:"المحتوي",
+        index_intro_contact:"للتواصل",
+        index_intro_back:"العوده",
+        index_intro_p:"ثور، الثورات هي حق الأحرار. الصراخ والمواجهة. لا تجعل من نفسك إنساناً عادياً. من حقك أن تعيش مهما كانت الظروف، فخذ حقك وسنأخذ حقنا لنفتخر به. أنت",
+        index_intro_h1:"اعرفني لقدرتي ، و ليس اعاقتي",
+        index_around_h2_1:"مهما كانت احتياجاتك، أنت فريد من نوعه",
+        index_around_p_1:"الشخص ذو الاحتياجات الخاصة هو شخص يولد بإعاقة تمنعه ​​من ممارسة حياته الطبيعية سواء كانت قصور فكري أو عصبي أو حسي، وتمنعه ​​هذه الإعاقة من المشاركة الكاملة والفعالة في المجتمع",
+        index_around_h2_2:"ليس هناك عائق مع الإرادة",
+        index_around_p_2:"من المعروف أن الأشخاص ذوي الاحتياجات الخاصة يعانون من مشاكل في وظائف الجسم، وصعوبة في الحركة، والقيام بالأنشطة، لذا فهو يحتاج إلى مساعدة من يعتني به ويعتني به، ويجب أن يتمتع بالرعاية الصحية ليمارس حياته الطبيعية",
+        
+        index_content_p1:"لا تستسلم أبدًا، أنت على وشك الوصول",
+        index_content_p2:"أنت أقوى مما تعلم",
+        index_content_p3:"لا تتوقف حتى تحصل على أهدافك",
+        index_content_p4:"هناك شيء فيك يحتاجه العالم",
+        index_contact_h5:"للتواصل معنا",
+        index_contact_p:"© 2021 جميع الحقوق محفوظة",
+        index_content_h1:"استمتع بالقراءة",
+
+
+
+
+
+
+        /*----------------------------------------------index------------------------------------------------------*/ 
+        one_intro_books:"الكتب",
+        one_intro_more_info:"للمزيد من المعلومات",
+        one_intro_user_opinion:"رأي المستخدم",
+        one_intro_back:"العوده",
+        one_intro_h5:"تبادل الكتب 2024",
+        one_intro_h1:"تبادل الكتب، تبادل الأفكار",
+        one_intro_p:"ما هو كتابك المفضل ؟",
+        one_books_h2:"اختر كتابك المفضل",
+        one_footer_h1_1:"للتواصل معنا",
+        one_footer_h1_2:"123.456.789",
+        one_footer_p:"تابعنا",
+        one_footer_a:"اقرأ المزيد",
+        one_user_opinion_p1:"هذا قسم مزيج من الكتب عن الأشخاص ذوي الاحتياجات الخاصة",
+        one_user_opinion_p2:"الكتب في هذا القسم جيدة جداً، أعجبني هذا القسم.",
+        one_user_opinion_p3:"هذه أفضل الكتب عندي، الكتب مهمة جدا ورائعة.",
+        one_user_opinion_p4:"وهو قسم إنساني مهم جدًا ومهم لتوعية المجتمع.",
+        one_more_info_p1:"لا تبحث عما ينقصك، بل عما هو كامل ومحزن",
+        one_more_info_p2:"صديقي، يمكنك أن تفعل ذلك طالما أنك تتنفس",
+        one_more_info_p3:"امنح نفسك الفرصة للمحاولة ولا أعتقد أنك ستخذل نفسك أبدًا.",
+        one_more_info_p4:"اكسر القواعد، اقفز، أنت حالة ويجب عليك تحقيق الهدف.",
+        one_more_info_p5:"امنح الحياة لعجزك، وازرع الأمل في روحك، فالحياة حلوة لمن يريدها.",
+        one_more_info_p6:"عندما تنسى، سوف ينسى الناس إعاقتك.",
+        one_more_info_p7:"لا يحتاج الأشخاص ذوو الإعاقة إلى الشفقة بقدر حاجتهم إلى الأمل.",
+        one_more_info_p8:"الأمل غذاء العقل، والطموح نور الحياة، ولم تمنع الظروف أحداً.",
+        one_more_info_p9:"تحدي الحياة ودع الصحف تنشر صورك",
+        one_more_info_p10:"الإرادة هي أن تكون كما تريد، وليس كما تريد الحياة أن تكون.",
+        one_more_info_p11:"فقط الإحباط يمنع الوصول، والإعاقة عذر لمن لا يريد أن يأتي",
+        one_more_info_p12:"ليس لدي إعاقة، لدي موهبة مختلفة.",
+        one_more_info_p13:"لقد فتحت إعاقتي عيني لرؤية إمكاناتي الحقيقية.",
+        one_more_info_p14:"عندما يقول الجميع أنك لا تستطيع، فإن مثابرتك تقول: نعم، يمكنك ذلك.",
+        one_more_info_p15:"لا تظن أن الحياة خذلتك وحدك، خذل عجزك وانهض.",
+        one_more_info_p16:"الحياة مختبر فلا تدع غباء الآخرين يقودك إلى فخ الهزيمة",
+        one_more_info_h1:"الأشخاص ذوي الاحتياجات الخاصة",
+
+
+
+        /*=================================================================================*/ 
+        change:"تغيير السمه",
+        m_hassan_two_content_h1:"قراءه الكتب",
+
+    },
+    en:{
+        two_start_section_h1:"welcome to section people wih special needs",
+        two_start_section_p:"Being angry about my disability is a waste of time, one has to move forward and move forward in life, if you are constantly angry or complaining people will not have time for you.",
+        content:"content",
+        about:"about",
+        podcast:"podcast",
+        contact:"contact",
+        back:"back",
+        two_content_p1:"People of determination are people who can create miracles, light up life, are the stars of the world, and have a unique energy.",
+        two_content_p2:"If you do not mix with people of determination, you will not know the value of love <br>because they have enough serenity to be safe and happy",
+        two_content_a:"more read",
+        question:"read",
+        two_about_p1:"People of determination and disabilities are the stars capable of lighting up the world",
+        two_about_p2:"All human beings are responsible for populating the earth and creating life, so no one is without a role",
+        two_about_p3:"Good luck, determined person, There is a miracle on earth, and in heaven, your inability goes to heaven before you.",
+        two_about_p4:"One of the greatest values ​​of life is sanctifying man, sanctifying himself, and respecting everything God created",
+        two_podcast_h1:"podcast",
+        two_contact_h3:" to contact us",
+        /*---------------------------------------------------two--------------------------------------------------*/ 
+        index_intro_h2:"engoy reading",
+        index_intro_around:"around",
+        index_intro_content:"content",
+        index_intro_contact:"contact",
+        index_intro_back:"back",
+        index_intro_p:"Thor, revolutions are the right of free people. Scream and confront. Do not make yourself an ordinary human being. You have the right to live regardless of the circumstances, so take your right and we will take our right to be proud of it. You.",
+        index_intro_h1:"know me for any abilities , not my disabilit",
+        index_around_h2_1:"Whatever your needs, you are unique",
+        index_around_p_1:"A person with special needs is a person born with a handicap that impedes him from practicing his normal life, whether intellectual, nervous or sensory deficiencies, and this disability prevents him from full and effective participation in society",
+        index_around_h2_2:"There is no handicap with will",
+        index_around_p_2:"People with special needs are known to have problems with body functions, difficulty in movement, and carrying out activities, so he needs help from someone who takes care of him and takes care of him, and he must have health care to practice his normal life",
+        index_design_p:"My ears do not hear, but here my heart hears and sees, and it has my resolve. Every person in this life has special needs. I love you as you are",
+        index_content_p1:"never give up, You are almost there",
+        index_content_p2:"You are stronger than you know",
+        index_content_p3:"don't stop till you get your goals",
+        index_content_p4:"There's something in you that the world needs",
+        index_contact_h5:" to contact us",
+        index_contact_p:"© 2021 All Right Reserved",
+        index_content_h1:"enjoy reading",
+        /*----------------------------------------------------index-------------------------------------------------- */
+        one_intro_books:"books",
+        one_intro_more_info:"more info",
+        one_intro_user_opinion:"user opinion",
+        one_intro_back:"back",
+        one_intro_h5:"book exchange 2024",
+        one_intro_h1:"swap books,swap ideas",
+        one_intro_p:"what's your favorite book ?",
+        one_books_h2:"choose your favourite book",
+        one_footer_h1_1:"contacts us",
+        one_footer_h1_2:"123.456.789",
+        one_footer_p:"follow us",
+        one_footer_a:"more read",
+        one_user_opinion_p1:"that's mazig secton of books about people with speial needs",
+        one_user_opinion_p2:"books in this section is very good , i likedthis section.",
+        one_user_opinion_p3:" these are the best books , i have red the books are very important and wonderful.",
+        one_user_opinion_p4:" it is a humanitarian department are a very important one and is importantfor community awareness.",
+        one_more_info_p1:"Don't look for what you lack, but for what is complete and sad",
+        one_more_info_p2:"My friend, you can do it as long as you breathe",
+        one_more_info_p3:"Give yourself the chance to try and I don't think you will ever let yourself down.",
+        one_more_info_p4:"Break the rules, jump, you are a case and you must <br>achieve the goal.",
+        one_more_info_p5:"Give life to your helplessness, plant hope in your soul, for life is sweet for those who want it.",
+        one_more_info_p6:"When you forget, people will forget about your disability.",
+        one_more_info_p7:"Disabled people don't need pity as much as they need hope.",
+        one_more_info_p8:"Hope is the food of the mind, ambition is the light of life, and circumstances did not prevent anyone.",
+        one_more_info_p9:"Challenge life and let the newspapers publish your pictures",
+        one_more_info_p10:"Will is to be what you want, not what life wants it to be.",
+        one_more_info_p11:"Only frustration prevents access, and disability is an excuse for those who do not want to come",
+        one_more_info_p12:"I don't have a disability, I have a different talent.",
+        one_more_info_p13:"My disability has opened my eyes to see my true potential.",
+        one_more_info_p14:"When everyone says you can't, your perseverance says, Yes, you can.",
+        one_more_info_p15:"Do not think that life has let you down alone, let down your disability and rise up.",
+        one_more_info_p16:"Life is a laboratory, so do not let the stupidity of others lead you into the trap of defeat",
+        one_more_info_h1:"people with special needs",
+
+
+/*===================================================================================*/ 
+        change:"change theme",
+        m_hassan_two_content_h1:"books reading",
+
+    }
+}
+
+let selector=document.getElementById('language-selector');
+selector.onechange=updatelanguage();
+
+function updatelanguage(){
+    let language =selector.options[selector.selectedIndex].value;
+    let nodes =document.querySelectorAll('[data-lang]');
+    let i=nodes.length;
+
+    while(i--){
+        let key = nodes[i].getAttribute('data-lang');
+        nodes[i].innerHTML = lang[language][key]
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
